b374f3c817b97ef285a933c25a3e988f5d4c145c
baghelrahul159 <baghel.rahul159@gmail.com>
2020-10-18 17:39:17 +0530

    Unique BST

--- leetcode/recursion1/unique_bst.go
+++ leetcode/recursion1/unique_bst.go
@@ -1,39 +1,30 @@
 package recursion1
 
 func generateTrees(n int) []*TreeNode {
-	//breaking condition
-	//if n==1, only 1 tree is possible
-	if n == 1 {
-		return []*TreeNode{
-			&TreeNode{
-				Val:   1,
-				Left:  nil,
-				Right: nil,
-			},
+	if n == 0 {
+		return []*TreeNode{}
+	}
+	return generateTreeList(1, n)
+}
+
+func generateTreeList(start, end int) []*TreeNode {
+	list := make([]*TreeNode, 0)
+	if start > end {
+		list = append(list, nil)
+	}
+
+	for i := start; i <= end; i++ {
+		leftList := generateTreeList(start, i-1)
+		rightList := generateTreeList(i+1, end)
+		for _, left := range leftList {
+			for _, right := range rightList {
+				root := &TreeNode{Val: i}
+				root.Left = left
+				root.Right = right
+				list = append(list, root)
+			}
 		}
 	}
-	//if n==2, 2 trees are possible
-	if n == 2 {
-		return []*TreeNode{
-			&TreeNode{
-				Val:  1,
-				Left: nil,
-				Right: &TreeNode{
-					Val:   2,
-					Left:  nil,
-					Right: nil,
-				},
-			},
-			&TreeNode{
-				Val: 2,
-				Left: &TreeNode{
-					Val:   1,
-					Left:  nil,
-					Right: nil,
-				},
-				Right: nil,
-			},
-		}
-	}
-	return nil
+
+	return list
 }
