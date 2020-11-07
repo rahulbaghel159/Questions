a56b87b200275f8406162c0bcc85ebc7c2734778
baghelrahul159 <baghel.rahul159@gmail.com>
2020-11-07 19:08:06 +0530

    Adding solution for search in 2d matrix

--- leetcode/recursion2/search2DMatrix.go
+++ leetcode/recursion2/search2DMatrix.go
@@ -1,40 +1,60 @@
 package recursion2
 
-import "fmt"
-
 func searchMatrix(matrix [][]int, target int) bool {
-	fmt.Print("matrix")
-	fmt.Println(matrix)
-
 	if len(matrix) == 0 || len(matrix[0]) == 0 {
 		return false
 	}
 
-	n := len(matrix)
-	m := len(matrix[0])
+	m := len(matrix)
+	n := len(matrix[0])
 
-	pivot := matrix[m/2][n/2]
-
-	fmt.Println("pivot", pivot)
+	i, j := chosePivot(matrix, target)
+	pivot := matrix[i][j]
 
 	if pivot == target {
 		return true
 	}
 
-	if pivot > target {
-		return searchMatrix(copyMatrix(matrix, 0, m/2, 0, n/2), target) || searchMatrix(copyMatrix(matrix, 0, m/2, n/2, n), target) || searchMatrix(copyMatrix(matrix, m/2, m, 0, n/2), target)
-	}
-	return searchMatrix(copyMatrix(matrix, m/2+1, m, n/2+1, n), target) || searchMatrix(copyMatrix(matrix, m/2+1, m, 0, n/2+1), target) || searchMatrix(copyMatrix(matrix, 0, m/2+1, n/2+1, n), target)
+	return searchMatrix(copyMatrix(matrix, i, m-1, 0, j-1), target) || searchMatrix(copyMatrix(matrix, 0, i, j+1, n-1), target)
 }
 
 func copyMatrix(matrix [][]int, m1, m2, n1, n2 int) [][]int {
 	result := make([][]int, 0)
-	for i := m1; i < m2; i++ {
+	for i := m1; i <= m2; i++ {
 		temp := make([]int, 0)
-		for j := n1; j < n2; j++ {
+		for j := n1; j <= n2; j++ {
 			temp = append(temp, matrix[i][j])
 		}
 		result = append(result, temp)
 	}
 	return result
+}
+
+func chosePivot(matrix [][]int, target int) (i, j int) {
+	if len(matrix) == 0 || len(matrix[0]) == 0 {
+		return 0, 0
+	}
+
+	//method 1: chose middle element as pivot, this would reduce search in 3 quadrants
+	/*
+		m := len(matrix)
+		n := len(matrix[0])
+		return m / 2, n / 2
+	*/
+
+	//method 2: chose middle column and then iterate over all elements to find boundary pivot
+	// this would reduce search in 2 quadrants
+	m := len(matrix)
+	n := len(matrix[0])
+	for i := 0; i < m; i++ {
+		//pivot same as target, search success, return this as pivot
+		if matrix[i][n/2] == target {
+			return i, n / 2
+		}
+		//if element at index is greater than target, pivot found
+		if matrix[i][n/2] > target {
+			return i, n / 2
+		}
+	}
+	return m - 1, n / 2
 }
--- leetcode/recursion2/search2DMatrix_test.go
+++ leetcode/recursion2/search2DMatrix_test.go
@@ -12,45 +12,45 @@ func Test_searchMatrix(t *testing.T) {
 		args args
 		want bool
 	}{
-		// {
-		// 	name: "Test Case 1",
-		// 	args: args{
-		// 		matrix: [][]int{
-		// 			[]int{1, 4, 7, 11, 15},
-		// 			[]int{2, 5, 8, 12, 19},
-		// 			[]int{3, 6, 9, 16, 22},
-		// 			[]int{10, 13, 14, 17, 24},
-		// 			[]int{18, 21, 23, 26, 30},
-		// 		},
-		// 		target: 5,
-		// 	},
-		// 	want: true,
-		// },
-		// {
-		// 	name: "Test Case 2",
-		// 	args: args{
-		// 		matrix: [][]int{
-		// 			[]int{1, 4, 7, 11, 15},
-		// 			[]int{2, 5, 8, 12, 19},
-		// 			[]int{3, 6, 9, 16, 22},
-		// 			[]int{10, 13, 14, 17, 24},
-		// 			[]int{18, 21, 23, 26, 30},
-		// 		},
-		// 		target: 20,
-		// 	},
-		// 	want: false,
-		// },
-		// {
-		// 	name: "Test Case 3",
-		// 	args: args{
-		// 		matrix: [][]int{
-		// 			[]int{17, 24},
-		// 			[]int{26, 30},
-		// 		},
-		// 		target: 20,
-		// 	},
-		// 	want: false,
-		// },
+		{
+			name: "Test Case 1",
+			args: args{
+				matrix: [][]int{
+					[]int{1, 4, 7, 11, 15},
+					[]int{2, 5, 8, 12, 19},
+					[]int{3, 6, 9, 16, 22},
+					[]int{10, 13, 14, 17, 24},
+					[]int{18, 21, 23, 26, 30},
+				},
+				target: 5,
+			},
+			want: true,
+		},
+		{
+			name: "Test Case 2",
+			args: args{
+				matrix: [][]int{
+					[]int{1, 4, 7, 11, 15},
+					[]int{2, 5, 8, 12, 19},
+					[]int{3, 6, 9, 16, 22},
+					[]int{10, 13, 14, 17, 24},
+					[]int{18, 21, 23, 26, 30},
+				},
+				target: 20,
+			},
+			want: false,
+		},
+		{
+			name: "Test Case 3",
+			args: args{
+				matrix: [][]int{
+					[]int{17, 24},
+					[]int{26, 30},
+				},
+				target: 20,
+			},
+			want: false,
+		},
 		{
 			name: "Test Case 4",
 			args: args{
@@ -61,6 +61,30 @@ func Test_searchMatrix(t *testing.T) {
 				target: 20,
 			},
 			want: false,
+		},
+		{
+			name: "Test Case 5",
+			args: args{
+				matrix: [][]int{
+					[]int{-1, 3},
+				},
+				target: -1,
+			},
+			want: true,
+		},
+		{
+			name: "Test Case 6",
+			args: args{
+				matrix: [][]int{
+					[]int{1, 2, 3, 4, 5},
+					[]int{6, 7, 8, 9, 10},
+					[]int{11, 12, 13, 14, 15},
+					[]int{16, 17, 18, 19, 20},
+					[]int{21, 22, 23, 24, 25},
+				},
+				target: 25,
+			},
+			want: true,
 		},
 	}
 	for _, tt := range tests {
